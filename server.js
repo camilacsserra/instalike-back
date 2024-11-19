9b2a3b5947a808baab41315415a804384fb78592
camila.serra <camilasserra@gmail.com>
2024-11-19 13:27:48 +0000

    second 'Imersão Alura' day

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 import express from 'express';
 
+const posts = [
+    { id: 1, descricao: "Uma foto teste", imagem: "https://placecats.com/millie/300/150" },
+    { id: 2, descricao: "Paisagem montanhosa", imagem: "https://placecats.com/millie/300/150" },
+    { id: 3, descricao: "Gato fazendo panqueca", imagem: "https://placecats.com/millie/300/150" }
+  ];
+
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
-app.get('/', (req, res) => {
-    res.status(200).send('Boas vindas à imersão');
+app.get('/posts', (req, res) => {
+    res.status(200).json(posts);
+});
+function buscarFunction(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)});
+}
+
+app.get('/posts/:id', (req, res) => {
+    const index = buscarFunction(req.params.id);
+    res.status(200).json(posts[index]);
 });
